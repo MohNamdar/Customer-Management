b7e73a4cf3e033d07f5a11afbb01f285d7b357a1
Mohammad Namdar <MohNamdar01@gmail.com>
2024-10-08 15:09:27 +0330

    Initialize & config urls

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('club.urls'), name='club'),
+    path('orders/', include('order.urls'), name='order'),
+    path('shop/', include('shop.urls'), name='shop'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
