bcaf0357d1e61011bfeff1265ba7ce1c9ec1506a
Mohammad Namdar <MohNamdar01@gmail.com>
2024-10-08 14:14:30 +0330

    config customer model + Register in admin.py

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer
+
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'last_name', 'first_name', 'register_date', 'visit_count', 'kind')
+    search_fields = ('first_name', 'last_name', 'phone')
+    list_filter = ('phone', 'register_date', 'last_visit_date', 'kind')
--- club/models.py
+++ club/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django_jalali.db import models as jmodels
+
 
 # Create your models here.
+class Customer(models.Model):
+    class CustomerKind(models.TextChoices):
+        NORMAL = 'NORMAL', 'مشتری معمولی'
+        KNOWN = 'KNOWN', 'مشتری آشنا'
+        LOYAL = 'LOYAL', 'مشتری وفادار'
+
+    first_name = models.CharField(max_length=100, default='', verbose_name='نام')
+    last_name = models.CharField(max_length=100, default='', verbose_name='نام خانوادگی')
+    phone = models.CharField(max_length=11, verbose_name='شماره تلفن')
+    register_date = jmodels.jDateTimeField(auto_now_add=True, verbose_name='تاریخ پیوستن')
+    last_visit_date = jmodels.jDateTimeField(auto_now=True, verbose_name='تاریخ آخرین مراجه')
+    visit_count = models.PositiveIntegerField(default=0, verbose_name='تعداد مراجعه')
+    kind = models.CharField(max_length=6, choices=CustomerKind.choices, default=CustomerKind.NORMAL, verbose_name='نوع')
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+    class Meta:
+        ordering = ['-register_date']
+        indexes = [
+            models.Index(fields=['phone', '-register_date'])
+        ]
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
